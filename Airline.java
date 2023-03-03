75594b456ca95d2dd4c739951428d704731d4ba5
HannaanI <123582452+HannaanI@users.noreply.github.com>
2023-03-02 23:32:26 -0500

    Add files via upload

---
+++ Airline.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.airline;
+
+/**
+ *
+ * @author hannaan
+ */
+
+import java.util.Scanner;
+public class Airline {
+    
+    private boolean[] researvedSeat = new boolean[10];
+
+    public static void main(String[] args) {
+        int[] firstclass;
+        int[] economy;
+        
+        
+        for (int i = 0; i <= 10; i++ ){
+            
+            
+        }
+        
+    }
+}
